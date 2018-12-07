3619dc0266283a03a6b783994f431882cd4bfb86
Ben Mezger <me@benmezger.nl>
2018-12-07 16:35:08 -0200

    Feat: Send email and set email_sent to True.

--- manager/utils.py
+++ manager/utils.py
@@ -4,8 +4,10 @@ import logging
 import requests
 
 from django.conf import settings
+from django.apps import apps
 from django.template import Template, Context
 from django.template.loader import render_to_string
+from celery import shared_task
 
 
 logging.basicConfig(format="%(asctime)s - %(message)s", datefmt="%d-%b-%y %H:%M:%S")
@@ -17,16 +19,16 @@ def inject_to_content(content, context):
 
     return template.render(ctx)
 
-def send_mail(emails, payload):
+
+@shared_task(name="send_mail")
+def send_mail(emails, payload, model, model_id):
     logging.info(
-            "[send] | Request | Payload: (subject: {}, body: {}, mail_to: {})".format(
-            payload.get("subject", ""),
-            payload.get("body", ""),
-            str(emails),
+        "[send] | Request | Payload: (subject: {}, body: {}, mail_to: {})".format(
+            payload.get("subject", ""), payload.get("body", ""), str(emails)
         )
     )
 
-    if payload and not settings.DEBUG:
+    if payload and settings.DEBUG:
         data_payload = {
             "from": settings.MAILGUN_FROM,
             "to": emails,
@@ -40,11 +42,17 @@ def send_mail(emails, payload):
             data=data_payload,
         )
 
+        if response.status_code == 200:
+            Model = apps.get_model("manager.{}".format(model))
+            obj = Model.objects.get(pk=model_id)
+            obj.is_sent = True
+            obj.save()
+
         logging.info(
             "[send] | Response (HTTP Code {}) | Payload: {}".format(
                 response.status_code, response.text
             )
         )
 
-        return response.status_code
-    return
+        return {"status_code": response.status_code}
+    return {"status_code": 200}
